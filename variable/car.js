0e9bd4af2836ac86e0d9d9c215306d0ed2090148
Egor Razzorenov <razzorenov08@gmail.com>
2025-04-29 08:49:35 +0300

    Initial commit

---
+++ variable/car.js
@@ -0,0 +1,22 @@
+const carName = 'BMW';
+let maxCarSpeed = '200 km/h';
+let carOwner = '';
+
+// alert(carName);
+// alert(maxCarSpeed);
+// alert(carOwner);
+
+maxCarSpeed = '150 km/h';
+carOwner = 'First';
+console.log('У машины изменилась скорость', maxCarSpeed);
+console.log('У машины изменился владелец', maxCarSpeed);
+
+maxCarSpeed = '170 km/h';
+carOwner = 'Second';
+console.log('У машины изменилась скорость', maxCarSpeed);
+console.log('У машины изменился владелец', maxCarSpeed);
+
+maxCarSpeed = '220 km/h';
+carOwner = 'Third';
+console.log('У машины изменилась скорость', maxCarSpeed);
+console.log('У машины изменился владелец', maxCarSpeed);
---
+++ variable/index.js
@@ -0,0 +1,15 @@
+// JavaScript-код с созданием переменных
+/* Автор:
+Раззоренов Егор */
+
+const myName = 'Egor';
+const mySurname = 'Razzorenov';
+let myFavoriteDrink = 'Pineapple juice';
+let myFavoriteAnimal = 'Dog';
+let myFavoriteProgrammingLanguage = 'JavaScript';
+
+console.log(myName);
+console.log(mySurname);
+console.log(myFavoriteDrink);
+console.log(myFavoriteAnimal);
+console.log(myFavoriteProgrammingLanguage);
