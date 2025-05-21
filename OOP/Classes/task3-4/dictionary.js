aff27c7b383caf6be559a69ba35f2b08b1122210
NotLSK <razzorenov08@gmail.com>
2025-05-21 13:22:39 +0300

    Update tasks3/4. Additional task 4 (task-1)

--- OOP/Classes/task3-4/dictionary.js
+++ OOP/Classes/task3-4/dictionary.js
@@ -1,32 +1,53 @@
-// Задание №3
-class Dictionary { 
+// Задание №3 // Доп. задание №1
+class Dictionary {
+    #name;
+    #words;
+
     constructor(name) {
-        this.name = name;
-        this.words = {};
+        this.#name = name;
+        this.#words = {};
+    }
+
+    get mainName() {
+        return this.#name;
+    }
+
+    set mainName(name) {
+        this.#name = name;
+    }
+
+    get allWords() {
+        return this.#words;
+    }
+
+    _addNewWord(wordKey, wordObj) {
+        this.#words[wordKey] = wordObj;
     }
 
     add(word, description) {
-        if (!(word in this.words)) {
-            this.words[word] = {
-                word: word,
-                description: description
-            }
+        if (!this.#words[word]) {
+            this._addNewWord(word, {
+                word,
+                description,
+            });
+        } else {
+            console.log(`Слово ${word} уже существует в словаре!`)
         }
     }
 
     remove(word) {
-        delete this.words[word];
+        delete this.#words[word];
     }
 
     getWord(word) {
-        return this.words[word];
+        return this.#words[word];
     }
 
     showAllWords() {
-        Object.keys(this.words).forEach((item) => {
-            console.log(`${this.words[item].word} - ${this.words[item].description}`)
+        Object.keys(this.#words).forEach((item) => {
+            console.log(`${this.#words[item].word} - ${this.#words[item].description}`)
         })
     }
 }
 
-export {Dictionary};
+export { Dictionary };
--- OOP/Classes/task3-4/hard-words-dictionary.js
+++ OOP/Classes/task3-4/hard-words-dictionary.js
@@ -1,17 +1,16 @@
-import {Dictionary} from './dictionary.js';
+import { Dictionary } from './dictionary.js';
 
 class HardWordsDictionary extends Dictionary {
-    // constructor(name) {
-    //     super(name);
-    // }
 
     add(word, description) {
-        if (!(word in this.words)) {
-            this.words[word] = {
-                word: word,
-                description: description,
-                isDifficult: true
-            }
+        if (!this.allWords[word]) {
+            this._addNewWord(word, {
+                word,
+                description,
+                isDifficult: true,
+            });
+        } else {
+            console.log(`Слово ${word} уже существует в словаре!`)
         }
     }
 }
@@ -28,13 +27,19 @@ dictionary.showAllWords();
 console.log('\nЗадание 4');
 
 const hardWordsDictionary = new HardWordsDictionary('Сложные слова');
-    
+
 hardWordsDictionary.add('дилетант', 'Тот, кто занимается наукой или искусством без специальной подготовки, обладая только поверхностными знаниями.');
-    
+
 hardWordsDictionary.add('неологизм', 'Новое слово или выражение, а также новое значение старого слова.');
-    
-hardWordsDictionary.add('квант', 'Неделимая часть какой-либо величины в физике.'); 
-    
+
+hardWordsDictionary.add('квант', 'Неделимая часть какой-либо величины в физике.');
+
 hardWordsDictionary.remove('неологизм');
-    
+
 hardWordsDictionary.showAllWords();
+
+console.log('\n Доп. задание №1');
+console.log(hardWordsDictionary.mainName); // Сложные слова
+hardWordsDictionary.mainName = 'Новый Словарь';
+console.log(hardWordsDictionary.mainName); // Новый Словарь
+console.log(hardWordsDictionary.allWords); // выводит объект в котором есть слова
