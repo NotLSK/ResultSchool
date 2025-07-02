f2552ae9b3d8dba07e0f9345612cf47d20bd6de3
Egor Razzorenov <razzorenov08@gmail.com>
2025-07-02 09:49:18 +0300

    task-1 with react.createElement

--- React/task-1/src/App.jsx
+++ React/task-1/src/App.jsx
@@ -1,41 +1,111 @@
 import { useState } from 'react'
+import { createElement } from 'react';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
+/*
+Все наши функции это Императивный подход, так как мы описываем логику работы приложения (то есть описываем то что оно должно нам показать),
+а в return из App используется декларативный, так как мы просто вызываем функции и ждём от них определённого результата.
+Вообще сама по себе функция react.createElement(), я думаю, подразумевает декларативный подход и правильней было бы сказать,
+что императивный подход используется под капотом этой функции, а само её использование уже декларативный подход, т.к. мы просто описываем ожидаемый результат.
+В конечном итоге весь этот код всё равно представляет из себя декларативный подход, а вся императивность скрыта под капотом реакта.
+*/
+
+const App = () => {
 	const [count, setCount] = useState(0)
 
-	function currentDate() {
+	function getCurrentDateHTML() {
 		const currentDate = new Date(Date.now());
-		return currentDate.getFullYear();
+		const currentYear = currentDate.getFullYear();
+
+		return createElement(
+			'div',
+			{ className: 'year' },
+			`Current year: ${currentYear}`,
+		);
 	}
+
+	function getImageBlockHTML() {
+		return createElement(
+			'div',
+			null,
+			createElement(
+				'a',
+				{ href: "https://vite.dev", target: "_blank" },
+				createElement(
+					'img',
+					{ src: viteLogo, className: "logo", alt: "Vite logo" },
+				)
+			),
+			createElement(
+				'a',
+				{ href: "https://react.dev", target: "_blank" },
+				createElement(
+					'img',
+					{ src: reactLogo, className: "logo react", alt: "React logo" },
+				)
+			)
+		);
+	}
+
+	function getHeaderHTML() {
+		return createElement(
+			'h1',
+			null,
+			'Vite + React'
+		)
+	}
+
+	function getCardHTML() {
+		const codeBlock = () => {
+			return createElement(
+				'code',
+				null,
+				'src/App.jsx'
+			)
+		}
+
+		return createElement(
+			'div',
+			{ className: "card" },
+			createElement(
+				'button',
+				{
+					onClick: () => {
+						setCount((count) => {
+							count + 1;
+						})
+					},
+				},
+				`count is ${count}`
+			),
+			createElement(
+				'p',
+				null,
+				'Edit',
+				codeBlock(),
+				' and save to test HMR',
+			)
+		);
+	}
+
+	function getReadTheDocsHTML() {
+		createElement(
+			'p',
+			{ className: "read-the-docs" },
+			'Click on the Vite and React logos to learn more'
+		)
+	}
+
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.jsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
-			<div>
-				Current year: {currentDate()}
-			</div>
+			{getImageBlockHTML()}
+			{getHeaderHTML()}
+			{getCardHTML()}
+			{getReadTheDocsHTML()}
+			{getCurrentDateHTML()}
 		</>
 	)
 }
